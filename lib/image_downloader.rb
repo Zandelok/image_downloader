d3bca438223b866701a024f44575ec003797a22e
Zandelok <gleb.kirill98@gmail.com>
2022-11-07 15:34:00 +0300

    (NO-TICKET): rewrite url_validator. Improve image_downloader

--- lib/image_downloader.rb
+++ lib/image_downloader.rb
@@ -2,12 +2,9 @@
 
 require_relative 'application'
 require 'open-uri'
-require 'mechanize'
 
 class ImageDownloader < Application
   attr_reader :data
-
-  MAX_SIZE = 10_485_760
 
   def initialize(data)
     @data = data
@@ -20,30 +17,19 @@ class ImageDownloader < Application
   private
 
   def download_images(image_urls)
-    request = Mechanize.new
-    request.redirection_limit = 1
     threads = []
 
     image_urls.each do |image|
-      threads << Thread.new { http_download(image, request) }
+      threads << Thread.new { http_download(image) }
     end
+
     threads.each(&:join)
     p 'All allowed files were successfully downloaded'
   end
 
-  def http_download(link, request)
-    return unless remote_file_exists?(link, request) && valid_size?(link, request)
-
+  def http_download(link)
     File.open("public/images/#{link.split('/').last}", 'w+') do |file|
       file.write(URI.open(link).read)
     end
-  end
-
-  def remote_file_exists?(url, request)
-    request.get(url).response['content-type'].start_with?('image')
-  end
-
-  def valid_size?(url, request)
-    request.get(url).response['content-length'].to_i < MAX_SIZE
   end
 end
--- lib/url_validator.rb
+++ lib/url_validator.rb
@@ -1,32 +1,47 @@
 # frozen_string_literal: true
 
 require_relative 'application'
+require 'mechanize'
 
 class UrlValidator < Application
   attr_reader :file
+
+  MAX_SIZE = 10_485_760
 
   def initialize(file)
     @file = file
   end
 
   def call
-    parser(file)
+    validate_urls(file)
   end
 
   private
 
-  def parser(file_name)
+  def validate_urls(file_name)
+    request = Mechanize.new
+    request.redirection_limit = 1
+
     valid_urls = file_name.split.select { |link| valid_url?(link) }.uniq
-    if valid_urls.empty?
-      warn 'No reliable addresses were found'
+    image_urls = valid_urls.select { |link| remote_file_exists?(link, request) && valid_size?(link, request) }
+    if image_urls.empty?
+      warn 'No reliable files were found'
       exit
     end
 
-    valid_urls
+    image_urls
   end
 
   def valid_url?(url)
     url_regexp = %r{\A(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
     url =~ url_regexp ? true : false
+  end
+
+  def remote_file_exists?(url, request)
+    request.get(url).response['content-type'].start_with?('image')
+  end
+
+  def valid_size?(url, request)
+    request.get(url).response['content-length'].to_i < MAX_SIZE
   end
 end
