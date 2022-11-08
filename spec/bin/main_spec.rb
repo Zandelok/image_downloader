6fff9f861dc31b9c99adaad6e8daace87165f434
Zandelok <gleb.kirill98@gmail.com>
2022-11-08 18:38:31 +0300

    (NO-TICKET): spec improvements

--- spec/bin/main_spec.rb
+++ spec/bin/main_spec.rb
@@ -3,16 +3,32 @@
 require 'open3'
 
 RSpec.describe 'script' do
-  let(:stdoutput) { Open3.capture3("./bin/main #{file}") }
+  let(:stdoutput) { Open3.capture3("./bin/main #{file} #{path_for_downloads}") }
   let(:file) { 'spec/fixtures/test.txt' }
+  let(:path_for_downloads) { 'spec/fixtures/test_images/' }
   let(:result) { stdoutput[0] }
   let(:errors) { stdoutput[1] }
   let(:expected_result) { "\"All allowed files were successfully downloaded\"\n" }
+  let(:url) { 'https://s3.eu-central-1.amazonaws.com/nftlaunchpad.com-media/competition/BAYC/1.png' }
+
+  before do
+    stub_request(:get, url)
+  end
+
+  after do
+    FileUtils.rm_rf("#{path_for_downloads}/.", secure: true)
+  end
 
   context 'when file not provided' do
     let(:file) {}
 
-    it { expect(errors).to eq("Please, write file name\n") }
+    it { expect(errors).to eq("Please, write file name and path for downloads\n") }
+  end
+
+  context 'when path for downloads not provided' do
+    let(:path_for_downloads) {}
+
+    it { expect(errors).to eq("Please, write file name and path for downloads\n") }
   end
 
   context 'when invalid file' do
--- spec/lib/image_downloader_spec.rb
+++ spec/lib/image_downloader_spec.rb
@@ -3,11 +3,22 @@
 require_relative '../../lib/image_downloader'
 
 describe ImageDownloader do
+  let(:url) { 'https://s3.eu-central-1.amazonaws.com/nftlaunchpad.com-media/competition/BAYC/1.png' }
+
+  before do
+    stub_request(:get, url)
+  end
+
+  after do
+    FileUtils.rm_rf("#{path_for_downloads}/.", secure: true)
+  end
+
   describe '#call' do
-    subject { described_class.call(data) }
+    subject { described_class.call(data, path_for_downloads) }
+    let(:path_for_downloads) { 'spec/fixtures/test_images/' }
 
     context 'when valid urls exists' do
-      let(:data) { ['https://s3.eu-central-1.amazonaws.com/nftlaunchpad.com-media/competition/BAYC/1.png'] }
+      let(:data) { [url] }
 
       it 'download valid files' do
         expect(subject).to eq('All allowed files were successfully downloaded')
